a4e48db40c40e96b12f7a2f8852d74c6512014a5
Peter Loos <cpp@peterloos.de>
2025-08-25 13:46:11 +0200

    added exercises

--- C_All_In_One/Program.c
+++ C_All_In_One/Program.c
@@ -11,9 +11,28 @@ extern void control_structures();
 //    return 0;
 //}
 
+void eingabe()
+{
+    printf("Bitte Wert eingeben: ");
+
+    int n = 0;
+
+    scanf_s("%d", &n);   // & ist WICHTIG: Adresse
+
+    printf("n: %d\n", n);
+
+    // gerade / ungerade 
+    if (n % 2 == 0) {
+        printf("gerade\n");
+    }
+    else {
+        printf("ungerade\n");
+    }
+}
+
 int main()
 {
-    control_structures();
+    eingabe();
 
     return 0;
 }
